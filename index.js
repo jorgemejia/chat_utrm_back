a148716e1b69075abd0bc2bd582e0712ba949797
jorgemejia <jorge_mejia@outlook.com>
2022-02-28 16:37:22 -0500

    firs commit
    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require( 'express' );
+const dotenv = require( 'dotenv' );
+const app = express();
+
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+);
+
+app.use(express.json())
+
+// initialize configuration
+dotenv.config();
+
+const port = process.env.APP_PORT || 8081; // default port to listen
+
+// define a route handler for the default home page
+app.get( '/', ( req, res ) => {
+    res.send( "Hola Mundo!" );
+});
+
+app.post('/data', (req, res) => {
+    const data = req.body;
+    console.log('data from front', data);
+
+    for(let i in data.books) {
+        console.log('Libros leidos -> ', data.books[i].name );
+    }
+
+    res.json({
+        ok: true,
+        message: 'data recieved'
+    });
+});
+
+// start the Express server
+app.listen( port, () => {
+    console.log( `server started at http://localhost:${ port }` );
+} );
