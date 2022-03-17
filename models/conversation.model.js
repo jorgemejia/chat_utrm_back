2cdc703a19ad8ad587736c3e96bdb88e9589103b
Jorg <jorge_mejia@outlook.com>
2022-03-17 18:50:22 -0500

    Update conversation.model.js

--- models/conversation.model.js
+++ models/conversation.model.js
@@ -1,30 +1,30 @@
 import { Model, DataTypes } from 'sequelize';
-import { database } from '../config/database';
+import { database } from '../config/database.js';
 
 export class conversation extends Model { }
 
 conversation.init({
-      id: {
-         type: DataTypes.INTEGER,
-         allowNull: false,
-         primaryKey: true
-      },
-      from_id: {
-         type: DataTypes.INTEGER,
-         allowNull: true
-      },
-      to_id: {
-         type: DataTypes.INTEGER,
-         allowNull: true
-      },
-      uuid: {
-         type: DataTypes.STRING(255),
-         allowNull: true
-      }
-   }, {
-      sequelize: database,
-      tableName: 'conversation',
-      timestamps: false
-   }
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        from_id: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        to_id: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        uuid: {
+            type: DataTypes.STRING(255),
+            allowNull: true
+        }
+    }, {
+        sequelize: database,
+        tableName: 'conversation',
+        timestamps: false
+    }
 );
-
