b3a8e3b7ab6046879f59ece69c8a39f1c4f481be
Jorg <jorge_mejia@outlook.com>
2022-03-17 18:50:52 -0500

    Update messages.model.js

--- models/messages.model.js
+++ models/messages.model.js
@@ -1,38 +1,37 @@
 import { Model, DataTypes } from 'sequelize';
-import { database } from '../config/database';
+import { database } from '../config/database.js';
 
 export class messages extends Model { }
 
 messages.init({
-      id: {
-         type: DataTypes.INTEGER.UNSIGNED,
-         allowNull: false,
-         primaryKey: true,
-         comment: "null",
-         autoIncrement: true
-      },
-      conversation_id: {
-         type: DataTypes.INTEGER,
-         allowNull: true
-      },
-      user_id: {
-         type: DataTypes.INTEGER,
-         allowNull: true
-      },
-      msg: {
-         type: DataTypes.STRING(255),
-         allowNull: true
-      },
-      created_at: {
-         type: DataTypes.STRING,
-         allowNull: true
-      }
-   }, {
-      sequelize: database,
-      tableName: 'messages',
-      timestamps: false
-   }
+        id: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            allowNull: false,
+            primaryKey: true,
+            comment: "null",
+            autoIncrement: true
+        },
+        conversation_uuid: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        msg: {
+            type: DataTypes.STRING(255),
+            allowNull: true
+        },
+        created_at: {
+            type: DataTypes.STRING,
+            allowNull: true
+        }
+    }, {
+        sequelize: database,
+        tableName: 'messages',
+        timestamps: false
+    }
 );
 
 // messages.hasMany(messages, {sourceKey: 'conversation_id'})
-
