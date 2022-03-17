323912d2a1bbf8cd19c6e78b8dba5f80ea6bba1a
Jorg <jorge_mejia@outlook.com>
2022-03-17 18:53:01 -0500

    Update socket.io.js

--- socket/socket.io.js
+++ socket/socket.io.js
@@ -1,49 +1,50 @@
 import { Server } from 'socket.io';
 import { Op } from 'sequelize';
 import { UserModel } from '../models/user.model.js';
+import {messages} from '../models/message.model.js';
+import { conversation } from '../models/conversation.model.js';
+
 
 const io = new Server();
-
 
 export class SocketIo {
 
     startSocket(http) {
-
-        this.io = new Server(http, {cors: {
-            origin: '*', methods: ["GET", "POST"]
+        this.io = new Server(http, { cors: {
+                origin: '*', methods: ["GET", "POST"]
         }});
 
-        const users = {}
+        const users = {};
 
         this.io.on('connection',  (socket) => {
-           console.log('user connected to socket');
 
-           socket.on('login', async (user) => {
-               console.log('user logged in', socket.id);
-               console.log('user logged in', user);
+            console.log('new usr in socket');
 
-               await UserModel.update({socket_id: socket.id, online: true }, { where: { id: user.id }, logging: console.log });
+            socket.on('login', async (user) => {
+                console.log('new user logged', user);
+                await UserModel.update({ socket_id: socket.id, online: true }, { where: { id: user.id } });
+                const onlineUsers = await UserModel.findAll({ where: { online: true } });
+                this.io.emit('new-user-online', onlineUsers);
 
-               const onlineUsers = await UserModel.findAll({where: { online: true }, logging: console.log });
+            });
 
-               this.io.emit('new-user-online', onlineUsers);
-           });
+            socket.on('new-message', async (data) => {
+                console.log('incoming data', data);
+                const msgs = await messages.findOne({ where: { conversation_uuid: data.uuid, id: data.id } });
+                const conv = await conversation.findOne({ where: { uuid: data.uuid, from_id: data.from_id }});
+                const user = await UserModel.findOne({ where: { id: conv.to_id } });
 
-           socket.on('disconnect', async () => {
-               console.log('user ' + socket.id + ' disconnected');
+                // console.log('user to send', user);
+                this.io.to(user.socket_id).emit('new-message', msgs);
+            });
 
-               await UserModel.update({socket_id: socket.id, online: false }, { where: { socket_id: socket.id } });
-               const onlineUsers = await UserModel.findAll({where: { online: true, socket_id: socket.id } });
-               this.io.emit('new-user-online', onlineUsers);
-               // remove saved socket from users object
-               // delete users[socket.id];
-           });
 
+            socket.on('disconnect', async () => {
+                console.log('user disconnected', socket.id);
+                await UserModel.update({ socket_id: socket.id, online: false }, { where: { socket_id: socket.id } });
+                const onlineUsers = await UserModel.findAll({where: { online: true, socket_id: socket.id } });
+                this.io.emit('new-user-online', onlineUsers);
+            });
         });
-
     }
-}
-
-export class SocketIoEvents {
-
 }
